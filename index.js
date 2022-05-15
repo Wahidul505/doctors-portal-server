d36e4873648a5d38961d3555d574139341cbffa6
Wahidul30 <wahidul505@gmail.com>
2022-05-15 14:09:33 +0600

    implemented functionalities to make a user an admin and acccess the admin to do extra funtionalities

--- index.js
+++ index.js
@@ -37,6 +37,32 @@ async function run() {
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         const userCollection = client.db('doctors_portal').collection('users');
 
+        // endpoint for make an user an admin  
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                return res.send(result);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
+
+        // endpoint for put a new user into database 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
