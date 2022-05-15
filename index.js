aa1b847fb11ac71ec4be97f4276067f21da7e474
Wahidul30 <wahidul505@gmail.com>
2022-05-15 17:03:59 +0600

    commented for better undertanding

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nap5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// middleTire 
+// middleware for verify the JWT.If there is nothing on the authHeader it returns 401 status. If the authHeader is truthy and If verified it set the decoded email in request object. If doesn't verified it return 403 status with a message.
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -33,8 +33,11 @@ function verifyJWT(req, res, next) {
 async function run() {
     try {
         client.connect();
+        // collection for all the treatments 
         const treatmentCollection = client.db('doctors_portal').collection('treatments');
+        // collection for all the bookings of all users 
         const bookingCollection = client.db('doctors_portal').collection('bookings');
+        // collection for both all users and admins. Where admins has an extra property => 'role: admin' 
         const userCollection = client.db('doctors_portal').collection('users');
 
         // endpoint for make an user an admin  
@@ -55,6 +58,7 @@ async function run() {
             }
         });
 
+        // the endpoint is using in useAdmin component in client side for checking if a user is admin 
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
@@ -62,7 +66,7 @@ async function run() {
             res.send({ admin: isAdmin });
         })
 
-        // endpoint for put a new user into database 
+        // endpoint for inserting a new user and giving the user a verified token and prevent inserting for previous user into database 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -76,18 +80,13 @@ async function run() {
             res.send({ result, token });
         });
 
+        // endpoint for getting all the users to display in admin panel 
         app.get('/user', async (req, res) => {
             const users = await userCollection.find({}).toArray();
             res.send(users);
         })
 
-        app.get('/treatment', async (req, res) => {
-            const query = {};
-            const cursor = treatmentCollection.find(query);
-            const treatments = await cursor.toArray();
-            res.send(treatments);
-        });
-
+        // endpoint for insert a treatment booking into database 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
@@ -99,6 +98,7 @@ async function run() {
             return res.send({ success: true, result });
         });
 
+        // endpoint for getting all the bookings for a particular user, only if the user has the valid jwt 
         app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
             const query = { patient: patient };
@@ -106,7 +106,7 @@ async function run() {
             res.send(bookings);
         })
 
-
+        // endpoint for getting the treatments with available slots for a particular date 
         app.get('/available', async (req, res) => {
             const date = req.query.date || 'May 15, 2022';
             const treatments = await treatmentCollection.find({}).toArray();
